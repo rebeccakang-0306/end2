63ceb99d167e0f3c6e7f25a8eb2e80749a89629c
yinankang <yinank@student.unimelb.edu.au>
2021-05-02 12:32:59 +0800

    second

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as config from "request";
-import app from "../api";
-import * as pkg from "express";
+
+
 
 
 ReactDOM.render(
@@ -13,7 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-var port = process.env.PORT || config.port
-app.listen(port, function(){
-    console.log(`${pkg.name} listening on port ${port}`)
-});
